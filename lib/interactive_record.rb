c7121578f2c517d3f9ac546dc8d2c07c21be75f1
kodyclemens <kody.clemens@gmail.com>
2018-12-07 21:55:12 -0500

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,21 +2,18 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class InteractiveRecord
+
   def self.table_name
-    self.to_s.downcase.pluralize
+    "#{self.to_s.downcase}s"
   end
 
   def self.column_names
     DB[:conn].results_as_hash = true
-
     sql = "pragma table_info('#{table_name}')"
-
-    table_info = DB[:conn].execute(sql)
-    column_names = []
-    table_info.each do |row|
-      column_names << row["name"]
+    schema_hash = DB[:conn].execute(sql)
+    schema_hash.map do |hash|
+        hash['name']
     end
-    column_names.compact
   end
 
   def initialize(options={})
@@ -30,26 +27,32 @@ class InteractiveRecord
   end
 
   def col_names_for_insert
-    self.class.column_names.delete_if {|col| col == "id"}.join(", ")
+    self.class.column_names.tap { |column| column.delete("id") }.join(", ")
   end
 
   def values_for_insert
     values = []
-    self.class.column_names.each do |col_name|
-      values << "'#{send(col_name)}'" unless send(col_name).nil?
+    self.class.column_names.each do |column|
+      values << "'#{send(column)}'" unless send(column).nil?
     end
     values.join(", ")
   end
 
   def save
-    sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
+    sql = <<-SQL
+      INSERT INTO #{self.class.table_name} (#{self.col_names_for_insert})
+      VALUES (#{self.values_for_insert});
+    SQL
     DB[:conn].execute(sql)
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
+    self.id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{self.class.table_name}")[0][0]
   end
 
   def self.find_by_name(name)
-    sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
-    DB[:conn].execute(sql)
+    sql = <<-SQL
+      SELECT * FROM #{self.table_name}
+      WHERE name = ?;
+    SQL
+    DB[:conn].execute(sql, name)
   end
 
   def self.find_by(search_hash)
